0d89b5030cffb0653d6037a2fe1bbc433aa7a425
Kayla Ondracek <kayla.renee@gmail.com>
2018-03-23 10:00:51 -0700

    add doctor pictures to search results

--- src/ui.js
+++ src/ui.js
@@ -3,21 +3,23 @@ export function displayDoctors(response) {
   $(".output .doctor").remove();
   body.data.forEach(function(element) {
     $(".output").append("<div class='well doctor'>")
-    $(".output .doctor:last-child").append("<p>" + element.profile.first_name + " " + element.profile.last_name + "</p>");
+    $(".output .doctor:last-child").append("<div class='info'>")
+    $(".output .doctor:last-child .info").append("<p>" + element.profile.first_name + " " + element.profile.last_name + "</p>");
     if (element.practices[0].accepts_new_patients === true) {
-        $(".output .doctor:last-child").append("<p>Accepting new patients at " + element.practices[0].name + "</p>");
-        $(".output .doctor:last-child").append("<p class='address'>" + element.practices[0].visit_address.street + "</p>");
-        $(".output .doctor:last-child").append("<p class='address'>" + element.practices[0].visit_address.city + " " + element.practices[0].visit_address.state + " " + element.practices[0].visit_address.zip + "</p>");
-        $(".output .doctor:last-child").append("<p class='address'>" + element.practices[0].phones[0].number + "</p>");
+        $(".output .doctor:last-child .info").append("<p>Accepting new patients at " + element.practices[0].name + "</p>");
+        $(".output .doctor:last-child .info").append("<p class='address'>" + element.practices[0].visit_address.street + "</p>");
+        $(".output .doctor:last-child .info").append("<p class='address'>" + element.practices[0].visit_address.city + " " + element.practices[0].visit_address.state + " " + element.practices[0].visit_address.zip + "</p>");
+        $(".output .doctor:last-child .info").append("<p class='address'>" + element.practices[0].phones[0].number + "</p>");
       }
     if (element.specialties.length > 0) {
-      $(".output .doctor:last-child").append("<p>Specialties: </p>")
-      $(".output .doctor:last-child").append("<ul>")
+      $(".output .doctor:last-child .info").append("<p>Specialties: </p>")
+      $(".output .doctor:last-child .info").append("<ul>")
       element.specialties.forEach(function(specialty) {
-        $(".output .doctor:last-child ul").append("<li>" + specialty.actor + "</li>")
+        $(".output .doctor:last-child .info ul").append("<li>" + specialty.actor + "</li>")
       });
-
     }
+    $(".output .doctor:last-child").append("<div class='doctor-img'>")
+    $(".output .doctor:last-child .doctor-img").append('<img class="actual-doctor-img" src="' + element.profile.image_url + '"</>')
 
   //  $(".output").append("<p>" + element.practices[0].accepts_new_patients + "</p>");
   });
