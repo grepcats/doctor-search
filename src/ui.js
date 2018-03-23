bf774d78e28e5e45a3a4b0bdd349f290bb7dd3c9
Kayla Ondracek <kayla.renee@gmail.com>
2018-03-23 10:15:17 -0700

    handle scenarios where there are no search results

--- src/ui.js
+++ src/ui.js
@@ -1,28 +1,32 @@
 export function displayDoctors(response) {
   let body = JSON.parse(response);
-  $(".output .doctor").remove();
-  body.data.forEach(function(element) {
-    $(".output").append("<div class='well doctor'>")
-    $(".output .doctor:last-child").append("<div class='info'>")
-    $(".output .doctor:last-child .info").append("<p>" + element.profile.first_name + " " + element.profile.last_name + "</p>");
-    if (element.practices[0].accepts_new_patients === true) {
-        $(".output .doctor:last-child .info").append("<p>Accepting new patients at " + element.practices[0].name + "</p>");
-        $(".output .doctor:last-child .info").append("<p class='address'>" + element.practices[0].visit_address.street + "</p>");
-        $(".output .doctor:last-child .info").append("<p class='address'>" + element.practices[0].visit_address.city + " " + element.practices[0].visit_address.state + " " + element.practices[0].visit_address.zip + "</p>");
-        $(".output .doctor:last-child .info").append("<p class='address'>" + element.practices[0].phones[0].number + "</p>");
+  $(".output div").remove();
+  if (body.data.length > 0) {
+    body.data.forEach(function(element) {
+      $(".output").append("<div class='well doctor'>")
+      $(".output .doctor:last-child").append("<div class='info'>")
+      $(".output .doctor:last-child .info").append("<p>" + element.profile.first_name + " " + element.profile.last_name + "</p>");
+      if (element.practices[0].accepts_new_patients === true) {
+          $(".output .doctor:last-child .info").append("<p>Accepting new patients at " + element.practices[0].name + "</p>");
+          $(".output .doctor:last-child .info").append("<p class='address'>" + element.practices[0].visit_address.street + "</p>");
+          $(".output .doctor:last-child .info").append("<p class='address'>" + element.practices[0].visit_address.city + " " + element.practices[0].visit_address.state + " " + element.practices[0].visit_address.zip + "</p>");
+          $(".output .doctor:last-child .info").append("<p class='address'>" + element.practices[0].phones[0].number + "</p>");
+        }
+      if (element.specialties.length > 0) {
+        $(".output .doctor:last-child .info").append("<p>Specialties: </p>")
+        $(".output .doctor:last-child .info").append("<ul>")
+        element.specialties.forEach(function(specialty) {
+          $(".output .doctor:last-child .info ul").append("<li>" + specialty.actor + "</li>")
+        });
       }
-    if (element.specialties.length > 0) {
-      $(".output .doctor:last-child .info").append("<p>Specialties: </p>")
-      $(".output .doctor:last-child .info").append("<ul>")
-      element.specialties.forEach(function(specialty) {
-        $(".output .doctor:last-child .info ul").append("<li>" + specialty.actor + "</li>")
-      });
-    }
-    $(".output .doctor:last-child").append("<div class='doctor-img'>")
-    $(".output .doctor:last-child .doctor-img").append('<img class="actual-doctor-img" src="' + element.profile.image_url + '"</>')
+      $(".output .doctor:last-child").append("<div class='doctor-img'>")
+      $(".output .doctor:last-child .doctor-img").append('<img class="actual-doctor-img" src="' + element.profile.image_url + '"</>');
+    });
+  } else {
+    $(".output").append("<div class='error'>There are no doctors matching your search. Please change your criteria and try again.</div>");
+  }
 
-  //  $(".output").append("<p>" + element.practices[0].accepts_new_patients + "</p>");
-  });
+
 
   //console.log(body.data[1]);
 }
